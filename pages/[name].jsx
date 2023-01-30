8c56e8e666960e80e9d357fb4d3c977164528f16
Martin Padron <martinepadronv@gmail.com>
2023-01-30 15:50:16 -0400

    Agregando progress bar y flechita para echar para atrás

--- pages/[name].jsx
+++ pages/[name].jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import Tipo from "@/components/pokecard/Tipo";
 import Collection from "@/components/sprite/Collection";
 import Movimientos from "@/components/movimientos/Movimientos";
 import Stats from "@/components/stats/Stats";
+import { MdArrowBackIosNew } from "react-icons/md";
 
 export async function getStaticPaths() {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=649`);
@@ -37,12 +39,12 @@ const Name = ({ pokemon }) => {
 
   return (
     <>
-    <Head>
-      <title>{`${pokemon.name} - Pokémarto`}</title>
-    </Head>
+      <Head>
+        <title>{`${pokemon.name} - Pokémarto`}</title>
+      </Head>
       <main className="bg-black text-white flex flex-col gap-[4rem]">
         {/* Nombre y foto principal  */}
-        <section className="pt-[4rem]">
+        <section className="relative pt-[5rem]">
           <div className="w-11/12 mx-auto flex flex-col items-center justify-center gap-2">
             <h1 className="text-4xl uppercase">{pokemon.name}</h1>
             <h2 className="text-lg uppercase">{`ID: ${pokemon.id}`}</h2>
@@ -51,6 +53,11 @@ const Name = ({ pokemon }) => {
             </div>
             <Tipo pokemon={pokemon} />
           </div>
+
+          <Link href='/' className="absolute top-6 left-4 flex gap-2 items-center justify-center">
+            <MdArrowBackIosNew className=" text-3xl text-white block 2xl:text-5xl" />
+            <p className="block 2xl:text-xl">Volver</p>
+          </Link>
         </section>
 
         {/* Stats  */}
